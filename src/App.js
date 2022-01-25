6ab59bcd062705288551a70bdf86bc8a979fe6a6
Leo Kirasic <kirasicleo@gmail.com>
2022-01-25 16:36:43 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">CV App</header>
+    </div>
+  );
+}
+
+export default App;
